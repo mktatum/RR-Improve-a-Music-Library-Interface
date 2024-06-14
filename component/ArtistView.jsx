255a98cfa9ce1292bde81b4616cf606da48fdb6b
Melanie Allmond <melanie.allmond@gmail.com>
2024-06-14 19:35:47 -0400

    basic setup complete

---
+++ component/ArtistView.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData()
+    })
+}
